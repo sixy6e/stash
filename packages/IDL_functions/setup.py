d25bfab7166c8bca5decf660acfa52f944a6ff7d
Josh Sixsmith <josh.sixsmith@gmail.com>
2013-09-08 09:01:33 +1000

    Initial change to incorporate scripts located under tests/

--- packages/IDL_functions/setup.py
+++ packages/IDL_functions/setup.py
@@ -9,12 +9,13 @@ import sys
 # 2. python setup.py install
 
 # Gather up all the files we need.
-files = ['Src/IDL_Histogram.f90',]
-
+#files = ['Src/IDL_Histogram.f90', 'tests/unit_test_IDL_Hist.f90']
+#unit_test_IDL_Hist
 ## scypy_distutils Script
 from numpy.distutils.core import setup, Extension
+#from numpy.distutils.core import Extension
 from numpy.distutils import fcompiler
-
+#from setuptools import setup
 # need to incorporate a complete fail and exit if the gnu95 compiler
 # is not found.
 avail_fcompilers = fcompiler.available_fcompilers_for_platform()
@@ -25,18 +26,22 @@ extra_compile_args=['--fcompiler=gnu95']
     
 ## setup the python module
 setup(name="IDL_functions", # name of the package to import later
+      version='1.0',
+      author='Josh Sixsmith',
+      author_email='joshua.sixsmith@ga.gov.au, josh.sixsmith@gmail.com',
       ## Build fortran wrappers, uses f2py
-      ext_modules = [Extension('_idl_histogram',
-                               files,
+      ext_modules = [Extension('_idl_histogram', ['Src/IDL_Histogram.f90'],
+#                               files,
 #                              libraries=[],
 #                              library_dirs=[],
 #   			       include_dirs=['Src'],
 #                               extra_compile_args=extra_compile_args,
                                ),
-                     ],
+                     Extension('IDL_functions.tests.unit_test_IDL_Hist', ['tests/unit_test_IDL_Hist.f90']
+                    ) ],
       
 ##      ## Install these to their own directory
-     package_dir={'IDL_functions':'Lib'},
-     packages = ["IDL_functions"],
-      
+     package_dir = {'IDL_functions':'Lib', 'IDL_functions/tests':'tests'},
+     packages = ["IDL_functions", 'IDL_functions/tests'],
+     test_suite = 'IDL_functions.tests.unit_test_IDL_Hist' 
      )
