b701c2da29a1558ed94b029e0c3216a22a97a431
Josh Sixsmith <josh.sixsmith@gmail.com>
2013-09-08 09:27:56 +1000

    Formatting changes.

--- packages/IDL_functions/setup.py
+++ packages/IDL_functions/setup.py
@@ -1,26 +1,24 @@
 import sys
 
 # we should build this module in three steps:
-# unix:
+# Unix/Linux:
 # 1. python setup.py build --fcompiler=gnu95
 # 2. python setup.py test.py
 # If all tests have passed, then it is ok to install.
 # 3. python setup.py install
-# windows
+
+# Windows
 # 1. python setup.py build --fcompiler=gnu95 --compiler=mingw32
 # 2. python setup.py test.py
 # If all tests have passed, then it is ok to install.
 # 3. python setup.py install
 
-# Gather up all the files we need.
-#files = ['Src/IDL_Histogram.f90', 'tests/unit_test_IDL_Hist.f90']
-#unit_test_IDL_Hist
-## scypy_distutils Script
+# Use distutils setup rather than setuptools setup as we need to compile Fortran
+# code using F2PY
 from numpy.distutils.core import setup, Extension
-#from numpy.distutils.core import Extension
 from numpy.distutils import fcompiler
-#from setuptools import setup
-# need to incorporate a complete fail and exit if the gnu95 compiler
+
+# TODO Need to incorporate a complete fail and exit if the gnu95 compiler
 # is not found.
 avail_fcompilers = fcompiler.available_fcompilers_for_platform()
 if ('gnu95' not in avail_fcompilers):
@@ -33,19 +31,22 @@ setup(name="IDL_functions", # name of the package to import later
       version='1.0',
       author='Josh Sixsmith',
       author_email='joshua.sixsmith@ga.gov.au, josh.sixsmith@gmail.com',
-      ## Build fortran wrappers, uses f2py
-      ext_modules = [Extension('_idl_histogram', ['Src/IDL_Histogram.f90'],
-#                               files,
-#                              libraries=[],
-#                              library_dirs=[],
-#   			       include_dirs=['Src'],
-#                               extra_compile_args=extra_compile_args,
-                               ),
-                     Extension('IDL_functions.tests.unit_test_IDL_Hist', ['tests/unit_test_IDL_Hist.f90']
-                    ) ],
+      # Build fortran wrappers, uses f2py
+      #ext_modules = [Extension('_idl_histogram', ['Src/IDL_Histogram.f90'],
+      #                          files,
+      #                         libraries=[],
+      #                         library_dirs=[],
+      #  		       include_dirs=['Src'],
+      #                         extra_compile_args=extra_compile_args,
+      #                         ),
+      ext_modules = [
+                     Extension('_idl_histogram',['Src/IDL_Histogram.f90']),
+                     Extension('IDL_functions.tests.unit_test_IDL_Hist',['tests/unit_test_IDL_Hist.f90'])
+                    ],
       
-##      ## Install these to their own directory
+     ## Install these to their own directory
      package_dir = {'IDL_functions':'Lib', 'IDL_functions/tests':'tests'},
      packages = ["IDL_functions", 'IDL_functions/tests'],
      test_suite = 'IDL_functions.tests.unit_test_IDL_Hist' 
      )
+
