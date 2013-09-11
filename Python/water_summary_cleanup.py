7ce6609b8264429b28ecd7bddfb3b5304909b755
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2013-09-11 14:05:21 +1000

    Added comments to the datatype function.

--- Python/water_summary_cleanup.py
+++ Python/water_summary_cleanup.py
@@ -6,6 +6,30 @@ from IDL_functions import IDL_Histogram
 
 def summary_cleanup(array, min_value=1, max_value=4, min_pop_count=10):
     """
+    A function for removing pixel 'islands' from the water summary output.
+    Using the default parameters, pixel groups with less than 10 members
+    within the data range of (1 <= x <= 4) are removed from the original array.
+
+    :param array:
+        A 2-Dimensional numpy array.
+
+    :param min_value:
+        Default value of 1. The minimum pixel value to be included within the analysis.
+
+    :param max_value:
+        Default value of 4. The maximum pixel value to be included within the analysis.
+
+    :param min_pop_count:
+        Default value of 10. The minimum population size a group of pixels must be in order to be retained.
+
+    :return:
+        A copy of array with pixels satisfying the min_value/max_value/min_pop_count parameters removed.
+
+    :author:
+        Josh Sixsmith; joshua.sixsmith@ga.gov.au
+
+    :history:
+        *  2013/09/11: Created
     """
     dims = array.shape
     if (len(dims) != 2):
@@ -24,7 +48,7 @@ def summary_cleanup(array, min_value=1, max_value=4, min_pop_count=10):
     hist = h['histogram']
     ri   = h['ri']
 
-    wh = numpy.where(hist <= max_value)
+    wh = numpy.where(hist < max_value)
     for i in wh[0]:
         flat_array[ri[ri[i]:ri[i+1]]] = 0
 
