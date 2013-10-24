d4adae5579f66329ec0890970f0169f8e3806a9e
Josh Sixsmith <josh.sixsmith@gmail.com>
2013-10-24 22:36:19 +1100

    Added hist_equal to the test script.

--- packages/IDL_functions/test.py
+++ packages/IDL_functions/test.py
@@ -21,7 +21,8 @@ if __name__ == '__main__':
         os.chdir('build')
 
         # Find the unittest script
-        test_file = locate('unit_test_IDL_Hist.py', os.getcwd())[0]
+        test_file  = locate('unit_test_IDL_Hist.py', os.getcwd())[0]
+        test_file2 = 'unit_test_IDL_hist_equal.py'
 
         # Get the directory path that contains the unittest script and change to that directory
         dname = os.path.dirname(test_file)
@@ -33,4 +34,5 @@ if __name__ == '__main__':
 
         # Now execute the unittest script from the command line
         subprocess.call(['python', test_file])
+        subprocess.call(['python', test_file2])
 
