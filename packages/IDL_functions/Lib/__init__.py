3bf51c3c6d9bae30602d6e68710d771ae008f124
Josh Sixsmith <josh.sixsmith@gmail.com>
2013-10-24 22:46:25 +1100

    Function name change.

--- packages/IDL_functions/Lib/__init__.py
+++ packages/IDL_functions/Lib/__init__.py
@@ -1,4 +1,4 @@
-from IDL_Histogram import IDL_Histogram
+from IDL_histogram import histogram
 from IDL_hist_equal import hist_equal
 from IDL_array_indices import array_indices
 from IDL_bytscl import bytscl
