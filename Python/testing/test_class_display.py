f580de1e6f1ecedbf846e8734bdbc595c6015c03
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2013-12-19 09:31:18 +1100

    Added sphinx style documentation.

--- Python/testing/test_class_display.py
+++ Python/testing/test_class_display.py
@@ -8,6 +8,8 @@ from osgeo import gdal
 
 def get_class_colours(band, alpha=False, normal=False):
     """
+    Creates a numpy array containing the class colours used for a classified image. If normalised will return RGB as 0 -> 1, otherwise it is 0 -> 255.
+
     :param band:
         A GDAL band object.
 
@@ -36,9 +38,7 @@ def get_class_colours(band, alpha=False, normal=False):
     # Convert to a numpy array with dimensions [n,4]
     class_colours = numpy.array(class_colours, dtype='uint8') # RGB values are only 0 -> 255
 
-    if alpha:
-        # Do nothing
-    else:
+    if ~alpha:
         class_colours = class_colours[*,0:3] # Retreive only RGB parts.
 
     if normal:
@@ -46,8 +46,30 @@ def get_class_colours(band, alpha=False, normal=False):
 
     return class_colours
 
-def create_colour_table(colours, name='Custom_Class_Colours', n=None, register=False):
+def create_colour_map(colours, name='Custom_Class_Colours', n=None, register=False):
     """
+    Creates a colour map for use with matplotlib.
+
+    :param colours:
+        An [n,4] or [n,3] numpy array containing the colours desired for display.
+
+    :param name:
+        A string containing the name to be used for referencing the colour table. Default it 'Custom_Class_Colours'.
+
+    :param n:
+        The number of colour. If unset then n colours from colours array will be used.
+
+    :param register:
+        If set to True then the colour table is registered with cm (colormap) and can be retrieved via the get_cmap() method. Default is False.
+
+    :return:
+        A colour map object that can be used with matplotlib.
+
+    :author:
+        Josh Sixsmith; josh.sixsmith@gmail.com
+
+    :history:
+        * 18/12/2013: Created
     """
 
     cmap = col.ListedColormap(colours, name=name, N=n)
