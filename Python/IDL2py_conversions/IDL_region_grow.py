c667cc75f72dec602de9d1c654f77a938fd13851
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2013-12-20 12:20:39 +1100

    Major changes to bring into line with the EXELISVIS function. No creation of ROI and no ROI boolean parameter.

--- Python/IDL2py_conversions/IDL_region_grow.py
+++ Python/IDL2py_conversions/IDL_region_grow.py
@@ -4,25 +4,25 @@ import numpy
 from scipy import ndimage
 from IDL_functions import histogram
 
-def region_grow(array, seed, stdv_multiplier=None, ROI=False, All_Neighbors=False, threshold=None):
+def region_grow(array, roi, stdv_multiplier=None, ROI=False, All_Neighbors=False, threshold=None):
     """
     Grows a single pixel or a group of pixels into a region.
 
     Similar to IDL's REGION_GROW function. (Interactive Data Language, EXELISvis).
-    For the single pixel case, the seed and its neighbours are used to generate statistical thresholds by which to grow connected pixels. 
-    If the keyword 'ROI' is set to True, then the seed will be assumed to be a region of neighbouring pixels. Otherwise the region grow function will iterate through the seed points and grow them individually.
+    For the single pixel case, the roi and its neighbours are used to generate statistical thresholds by which to grow connected pixels. 
+    If the keyword 'ROI' is set to True, then the roi will be assumed to be a region of neighbouring pixels. Otherwise the region grow function will iterate through the roi points and grow them individually.
 
     :param array:
         A single 2D numpy array.
 
-    :param seed:
+    :param roi:
         A tuple containing a the location of a single pixel, or multiple pixel locations.
 
     :param stdv_multiplier:
         A value containing the standard deviation multiplier that defines the upper and lower threshold limits. Defaulted to None, in which case the min and max will be used as defining threshold limits.
 
     :param ROI:
-        If set to True, then the seed is assumed to be a region of neighbouring pixels, and gather stats from the ROI to perform the threholding. Defaults to False; eg pixels are not neighbouring and will iterate through all pixels contained in the seed.
+        If set to True, then the roi is assumed to be a region of neighbouring pixels, and gather stats from the ROI to perform the threholding. Defaults to False; eg pixels are not neighbouring and will iterate through all pixels contained in the roi.
 
     :param All_Neighbours:
         If set to True, then all 8 neighbours will be used to search for connectivity. Defaults to False (only the 4 immediate neighbours are used for connectivity).
@@ -38,7 +38,7 @@ def region_grow(array, seed, stdv_multiplier=None, ROI=False, All_Neighbors=Fals
        * 12/12/2013: Re-written and adapted for the IDL_functions suite.
 
     :notes:
-        The keyword ROI is an addition to IDL's version of REGION_GROW, in that a series of seed points can be provided for growing, not just a single ROI.
+        The keyword ROI is an addition to IDL's version of REGION_GROW, in that a series of roi points can be provided for growing, not just a single ROI.
         IDL will return a vector of pixel indices representing the grown regions. Numpy can use a boolean array to index, as such a boolean array will be returned. This also makes it easier to return multiple grown regions within a single boolean array.
 
     :copyright:
@@ -98,26 +98,37 @@ def region_grow(array, seed, stdv_multiplier=None, ROI=False, All_Neighbors=Fals
         Calculates the upper and lower thresholds via the ROI of an array and a standard deviation multiplier.
         """
         print 'case_three'
-        stdv  = numpy.std(array[roi], ddof=1) # Sample standard deviation
-        limit = stdv_multiplier * stdv
-        mean  = numpy.mean(array[roi])
+
+        # For the case of a single pixel ROI, the standard deviation would be undefined
+        # So set the mean to equal the pixel value and stdv to 0.0
+        if (roi[0].shape == 1):
+            mean = array[roi]
+            stdv = 0.0
+        else:
+            stdv  = numpy.std(array[roi], ddof=1) # Sample standard deviation
+            limit = stdv_multiplier * stdv
+            mean  = numpy.mean(array[roi])
+
         upper = mean + limit
         lower = mean - limit
         print (upper,lower)
 
         return (upper,lower)
 
-    if len(array.shape) != 2:
-        raise Exception('Input array needs to be 2D in shape')
+    if (len(array.shape) != 2):
+        raise Exception('Input array needs to be 2D in shape!')
 
-    if type(seed) != tuple:
-        raise Exception('Seed must be a tuple')
+    if ((type(roi) != list) | (type(roi) != tuple)):
+        raise Exception('ROI must be of type tuple or type list containing (ndarray,ndarray) or [ndarray,ndarray]!')
 
-    if len(seed) != 2:
-        raise Exception('Seed must be of length 2')
+    if (type(roi[0]) != numpy.ndarray):
+        raise Exception('ROI must be of type ndarray for tuple (ndarray,ndarray) or list [ndarray,ndarray] style of index!')
 
-    if type(All_Neighbours) != bool:
-        raise Exception('All_Neighbours keyword must be of type bool')
+    if (len(roi) != 2):
+        raise Exception('ROI must be of length 2!')
+
+    if (type(All_Neighbours) != bool):
+        raise Exception('All_Neighbours keyword must be of type bool!')
 
     case_of = {
                 '1' : case_one,
@@ -149,63 +160,63 @@ def region_grow(array, seed, stdv_multiplier=None, ROI=False, All_Neighbors=Fals
     # Create the array that will hold the grown region
     grown_regions = numpy.zeros(dims, dtype='bool').flatten()
 
-    if (type(seed[0]) == numpy.ndarray) & ROI == False:
-       loop = range(len(seed[0]))
-    else:
-       loop = range(1)
+    #if (type(roi[0]) == numpy.ndarray) & ROI == False:
+    #   loop = range(len(roi[0]))
+    #else:
+    #   loop = range(1)
 
-    # The following loop can be very time consuming if there are lots of seed points!!!
-    # An ROI is calculated for every seed point
-    for i in loop:
+    # The following loop can be very time consuming if there are lots of roi points!!!
+    # An ROI is calculated for every roi point
+    #for i in loop:
 
-        if ROI == False:
-            # Find the seed's neighbours
-            x   = numpy.arange(9) % 3 + (seed[1][i] - 1)
-            y   = numpy.arange(9) / 3 + (seed[0][i] - 1)
-            roi = (y,x)
+    #    if ROI == False:
+    #        # Find the roi's neighbours
+    #        x   = numpy.arange(9) % 3 + (roi[1][i] - 1)
+    #        y   = numpy.arange(9) / 3 + (roi[0][i] - 1)
+    #        roi = (y,x)
 
-            # Check if any parts of the roi are outside the image
-            bxmin = numpy.where(roi[1] < 0)
-            bymin = numpy.where(roi[0] < 0)
-            bxmax = numpy.where(roi[1] >= dims[1])
-            bymax = numpy.where(roi[0] >= dims[0])
+    #        # Check if any parts of the roi are outside the image
+    #        bxmin = numpy.where(roi[1] < 0)
+    #        bymin = numpy.where(roi[0] < 0)
+    #        bxmax = numpy.where(roi[1] >= dims[1])
+    #        bymax = numpy.where(roi[0] >= dims[0])
 
-            # Change if roi co-ordinates exist outside the image domain.
-            roi[1][bxmin] = 0
-            roi[0][bymin] = 0
-            roi[1][bxmax] = dims[1]-1
-            roi[0][bymax] = dims[0]-1
-        else:
-            roi = seed
+    #        # Change if roi co-ordinates exist outside the image domain.
+    #        roi[1][bxmin] = 0
+    #        roi[0][bymin] = 0
+    #        roi[1][bxmax] = dims[1]-1
+    #        roi[0][bymax] = dims[0]-1
+    #    else:
+    #        roi = roi
 
-        print roi
-        print array[roi]
+    print roi
+    print array[roi]
 
-        upper, lower = case_of[case](array, roi)
+    upper, lower = case_of[case](array, roi)
 
-        # Create the mask via the thresholds
-        mask = (array >= lower) & (array <= upper)
+    # Create the mask via the thresholds
+    mask = (array >= lower) & (array <= upper)
 
-        # The label function segments the image into contiguous blobs
-        label_array, num_labels = ndimage.label(mask, structure=s)
+    # The label function segments the image into contiguous blobs
+    label_array, num_labels = ndimage.label(mask, structure=s)
 
-        # Find the labels associated with the roi
-        labels  = label_array[roi]
-        mx_lab  = numpy.max(labels)
-        ulabels = (numpy.unique(labels[labels > 0])).tolist() # Convert to list; Makes for neater indexing
-        print labels
-        print ulabels
+    # Find the labels associated with the roi
+    labels  = label_array[roi]
+    mx_lab  = numpy.max(labels)
+    ulabels = (numpy.unique(labels[labels > 0])).tolist() # Convert to list; Makes for neater indexing
+    print labels
+    print ulabels
 
-        # Generate a histogram to find the label locations, excluding zero (background)
-        # num_labels can be used for 'max' as the labeling function should return labels 0 through to n without skipping a value
-        h = histogram(label_array.flatten(), min=0, max=mx_lab, reverse_indices='ri')
-        hist = h['histogram']
-        ri = h['ri']
+    # Generate a histogram to find the label locations, excluding zero (background)
+    # num_labels can be used for 'max' as the labeling function should return labels 0 through to n without skipping a value
+    h = histogram(label_array.flatten(), min=0, max=mx_lab, reverse_indices='ri')
+    hist = h['histogram']
+    ri = h['ri']
 
-        for lab in ulabels:
-            if hist[lab] == 0:
-                continue
-            grown_regions[ri[ri[lab]:ri[lab+1]]] = True
+    for lab in ulabels:
+        if hist[lab] == 0:
+            continue
+        grown_regions[ri[ri[lab]:ri[lab+1]]] = True
 
     return grown_regions.reshape(dims)
 
