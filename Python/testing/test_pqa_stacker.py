51c989ac056c6973c6aadda3c36776fd50e40401
Josh Sixsmith <joshua.sixsmith@ga.gov.au>
2014-03-04 14:41:29 +1100

    In a working state. Now reflects saturation analysis. Possible name change coming.

--- Python/testing/test_pqa_stacker.py
+++ Python/testing/test_pqa_stacker.py
@@ -1,13 +1,14 @@
-'''
-Created on 21/02/2013
-
-@author: u76345
-'''
+#!/usr/bin/env python
 import os
 import sys
 import logging
+import errno
 from datetime import datetime, time
+import pdb
+import re
+import numpy
 
+from osgeo import gdal
 from stacker import Stacker
 from ULA3.utils import log_multiline
 
@@ -29,10 +30,28 @@ def extractPQFlags(array, flags=[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1], invert=False)
     # Specific bit positions for each PQ flag
     bits = [1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384, 32768]
 
+    bit_shift = {1 : 0,
+                 2 : 1,
+                 4 : 2,
+                 8 : 3,
+                 16 : 4,
+                 32 : 5,
+                 64 : 6,
+                 128 : 7,
+                 256 : 8,
+                 512 : 9,
+                 1024 : 10,
+                 2048 : 11,
+                 4096 : 12,
+                 8192 : 13,
+                 16384 : 14,
+                 32768 : 15
+                }
+
     # image dimensions
     dims = array.shape
     if len(dims) != 2:
-        return 'Error'
+        raise Exception('Error. Array dimensions must be 2D, not %i' %len(dims))
 
     rows = dims[0]
     cols = dims[1]
@@ -45,167 +64,253 @@ def extractPQFlags(array, flags=[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1], invert=False)
     pq_flags = numpy.zeros((n_flags,rows,cols), dtype='bool')
 
     for i in range(n_flags):
-        pq_flags[i] = (array >> flags[i]) == 1
+        flag = flags[i]
+        pq_flags[i] = (array & flag) >> bit_shift[flag]
 
     if invert:
         return ~pq_flags
     else:
         return pq_flags
-                
-class PQAStacker(Stacker):
-    """ Subclass of Stacker
-    Used to implement specific functionality to create stacks of derived datasets.
+
+def pqSaturation(file_path):
     """
-    def derive_datasets(self, input_dataset_info, stack_output_info, tile_type_info):
-        """ Overrides abstract function in stacker class. Called in Stacker.stack_derived() function. 
-        
-        Arguments:
-            input_dataset_info: Dict keyed by processing level (e.g. ORTHO, NBAR, PQA, DEM)
-                containing all tile info which can be used within the function
-                A sample is shown below:
-                
-input_dataset_info = {'NBAR': {'band_name': 'Visible Blue',
-    'band_tag': 'B10',
-    'end_datetime': datetime.datetime(2000, 2, 9, 23, 46, 36, 722217),
-    'end_row': 77,
-    'level_name': 'NBAR',
-    'nodata_value': -999L,
-    'path': 91,
-    'satellite_tag': 'LS7',
-    'sensor_name': 'ETM+',
-    'start_datetime': datetime.datetime(2000, 2, 9, 23, 46, 12, 722217),
-    'start_row': 77,
-    'tile_layer': 1,
-    'tile_pathname': '/g/data/v10/datacube/EPSG4326_1deg_0.00025pixel/LS7_ETM/150_-025/2000/LS7_ETM_NBAR_150_-025_2000-02-09T23-46-12.722217.tif'},
-'ORTHO': {'band_name': 'Thermal Infrared (Low Gain)',
-     'band_tag': 'B61',
-     'end_datetime': datetime.datetime(2000, 2, 9, 23, 46, 36, 722217),
-     'end_row': 77,
-     'level_name': 'ORTHO',
-     'nodata_value': 0L,
-     'path': 91,
-     'satellite_tag': 'LS7',
-     'sensor_name': 'ETM+',
-     'start_datetime': datetime.datetime(2000, 2, 9, 23, 46, 12, 722217),
-     'start_row': 77,
-     'tile_layer': 1,
-     'tile_pathname': '/g/data/v10/datacube/EPSG4326_1deg_0.00025pixel/LS7_ETM/150_-025/2000/LS7_ETM_ORTHO_150_-025_2000-02-09T23-46-12.722217.tif'},
-'PQA': {'band_name': 'Pixel Quality Assurance',
-    'band_tag': 'PQA',
-    'end_datetime': datetime.datetime(2000, 2, 9, 23, 46, 36, 722217),
-    'end_row': 77,
-    'level_name': 'PQA',
-    'nodata_value': None,
-    'path': 91,
-    'satellite_tag': 'LS7',
-    'sensor_name': 'ETM+',
-    'start_datetime': datetime.datetime(2000, 2, 9, 23, 46, 12, 722217),
-    'start_row': 77,
-    'tile_layer': 1,
-    'tile_pathname': '/g/data/v10/datacube/EPSG4326_1deg_0.00025pixel/LS7_ETM/150_-025/2000/LS7_ETM_PQA_150_-025_2000-02-09T23-46-12.722217.tif'}
-}                
-                
-        Arguments (Cont'd):
-            stack_output_info: dict containing stack output information. 
-                Obtained from stacker object. 
-                A sample is shown below
-                
-stack_output_info = {'x_index': 144, 
-                      'y_index': -36,
-                      'stack_output_dir': '/g/data/v10/tmp/ndvi',
-                      'start_datetime': None, # Datetime object or None
-                      'end_datetime': None, # Datetime object or None 
-                      'satellite': None, # String or None 
-                      'sensor': None} # String or None 
-                      
-        Arguments (cont'd):
-            tile_type_info: dict containing tile type information. 
-                Obtained from stacker object (e.g: stacker.tile_type_info) after instantiation. 
-                A sample is shown below
-                
-tile_type_info = {'crs': 'EPSG:4326',
-    'file_extension': '.tif',
-    'file_format': 'GTiff',
-    'format_options': 'COMPRESS=LZW,BIGTIFF=YES',
-    'tile_directory': 'EPSG4326_1deg_0.00025pixel',
-    'tile_type_id': 1L,
-    'tile_type_name': 'Unprojected WGS84 1-degree at 4000 pixels/degree',
-    'unit': 'degree',
-    'x_origin': 0.0,
-    'x_pixel_size': Decimal('0.00025000000000000000'),
-    'x_pixels': 4000L,
-    'x_size': 1.0,
-    'y_origin': 0.0,
-    'y_pixel_size': Decimal('0.00025000000000000000'),
-    'y_pixels': 4000L,
-    'y_size': 1.0}
-                            
-        Function must create one or more GDAL-supported output datasets. Useful functions in the
-        Stacker class include Stacker.get_pqa_mask(), but it is left to the coder to produce exactly
-        what is required for a single slice of the temporal stack of derived quantities.
-            
-        Returns:
-            output_dataset_info: Dict keyed by stack filename
-                containing filenames of GDAL-supported output datasets created by this function.
-                Note that the key(s) will be used as the output filename for the VRT temporal stack
-                and each dataset created must contain only a single band.
-        """
-        # Replace this with code to do fancy stuff
-        # Use the code for the Stacker.derive_datasets() as a template
+    """
 
-        assert type(input_dataset_dict) == dict, 'nbar_dataset_dict must be a dict'
+    # Open the PQ dataset
+    ds = gdal.Open(file_path)
 
-        pqa_dict = input_dataset_dict['PQA']
-        if pqa_dict is not None:
-            pqaPath = pqa_dict['tile_pathname']
-        satellite = stack_output_info['satellite']
-        sensor = stack_output_info['sensor']
+    # Number of bands, lines, samples
+    nb = ds.RasterCount
+    nl = ds.RasterYSize
+    ns = ds.RasterXSize
 
-        cellLat = stack_output_info['y_index']
-        cellLon = stack_output_info['x_index']
-        cellStartTime = input_dataset_dict['NBAR']['start_datetime']
+    # Get the projection and geotransform parameters
+    prj  = ds.GetProjection()
+    geoT = ds.GetGeoTransform()
 
-        pqaDataset = gdal.Open(pqaPath)
-        assert pqaDataset, 'Unable to open PQA GeoTIFF file %s' % pqaPath
-        pqaBand = pqaDataset.ReadAsArray()
-        pqaDataset.FlushCache()
-        pqaDataset = None
+    sat1 = numpy.zeros((2,nl,ns), dtype='uint16')
+    sat2 = numpy.zeros((2,nl,ns), dtype='uint16')
+    sat3 = numpy.zeros((2,nl,ns), dtype='uint16')
+    sat4 = numpy.zeros((2,nl,ns), dtype='uint16')
+    sat5 = numpy.zeros((2,nl,ns), dtype='uint16')
+    sat7 = numpy.zeros((2,nl,ns), dtype='uint16')
 
-        if sensor == 'TM':
-            pq_flags = extractPQFlags(pqaBand, flags=[1,1,1,1,1,1,0,1,0,0,0,0,0,0,0,0], invert=True)
-        elif sensor == 'ETM+':
-            pq_flags = extractPQFlags(pqaBand, flags=[1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0], invert=True)
+    # Special case for thermal
+    satTherm = numpy.zeros((3,nl,ns), dtype='uint16')
 
-        pqaBand = None
+    # Case switch for saturation lookup
+    sat_idx_map = {'TM' : [sat1[0], sat2[0], sat3[0], sat4[0], sat5[0], satTherm[0], sat7[0]],
+                   'ETM+' : [sat1[1], sat2[1], sat3[1], sat4[1], sat5[1], satTherm[1], satTherm[2], sat7[1]]
+                  }
+                  
+    # Loop over the time series
+    for i in range(1, nb + 1):
+        # Band object
+        band = ds.GetRasterBand(i)
 
-        return Stacker.derive_datasets(self, input_dataset_info, stack_output_info, tile_type_info)
-            
+        # PQ data
+        PQdata = band.ReadAsArray()
+        band.FlushCache()
+
+        # Metadata
+        md = band.GetMetadata_List()
+
+        # Get the sensor
+        if 'satellite_tag=LS5' in md:
+            sensor = 'TM'
+        else:
+            sensor = 'ETM+'
+
+        # Get the saturation flags
+        if sensor == 'TM': # Landsat 5
+            pq_flags = extractPQFlags(PQdata, flags=[1,1,1,1,1,1,0,1,0,0,0,0,0,0,0,0], invert=True)
+        else: # Landsat 7
+            pq_flags = extractPQFlags(PQdata, flags=[1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0], invert=True)
+
+        #if i == 1:
+        #    write_img(pq_flags.astype('uint8'), name='test_pq_bits')
+
+        # Update the saturation counts for each band
+        for i in range(len(sat_idx_map[sensor])):
+            sat_idx_map[sensor][i] += pq_flags[i]
+
+    return (sat1, sat2, sat3, sat4, sat5, satTherm, sat7, prj, geoT)
+
+def write_img(array, name='', format='ENVI', projection=None, geotransform=None):
+    """
+    Write a 2D/3D image to disk using GDAL.
+
+    :param array:
+        A 2D/3D Numpy array.
+
+    :param name:
+        A string containing the output file name.
+
+    :param format:
+        A string containing a GDAL compliant image format. Default is 'ENVI'.
+
+    :param projection:
+        A variable containing the projection information of the array.
+
+    :param geotransform:
+        A variable containing the geotransform information for the array.
+
+    :author:
+        Josh Sixsmith, joshua.sixsmith@ga.gov.au
+
+    :history:
+        * 04/09/2013--Created
+    """
+    dims   = array.shape
+    if (len(dims) == 2):
+        samples = dims[1]
+        lines   = dims[0]
+        bands   = 1
+    elif (len(dims) == 3):
+        samples = dims[2]
+        lines   = dims[1]
+        bands   = dims[0]
+    else:
+        print 'Input array is not of 2 or 3 dimensions!!!'
+        print 'Array dimensions: ', len(dims)
+        return
+
+    dtype  = datatype(array.dtype.name)
+    driver = gdal.GetDriverByName(format)
+    outds  = driver.Create(name, samples, lines, bands, dtype)
+
+    if (projection != None):
+        outds.SetProjection(projection)
+
+    if (geotransform != None):
+        outds.SetGeoTransform(geotransform)
+
+    if (bands > 1):
+        for i in range(bands):
+            band   = outds.GetRasterBand(i+1)
+            band.WriteArray(array[i])
+            band.FlushCache()
+    else:
+        band   = outds.GetRasterBand(1)
+        band.WriteArray(array)
+        band.FlushCache()
+
+    outds = None
+
+def datatype(val):
+    """
+    Provides a map to convert a numpy datatype to a GDAL datatype.
+
+    :param val:
+        A string numpy datatype identifier, eg 'uint8'.
+
+    :return:
+        An integer that corresponds to the equivalent GDAL data type.
+
+    :author:
+        Josh Sixsmith, joshua.sixsmith@ga.gov.au
+    """
+    instr = str(val)
+    return {
+        'uint8'     : 1,
+        'uint16'    : 2,
+        'int16'     : 3,
+        'uint32'    : 4,
+        'int32'     : 5,
+        'float32'   : 6,
+        'float64'   : 7,
+        'complex64' : 8,
+        'complex64' : 9,
+        'complex64' : 10,
+        'complex128': 11,
+        }.get(instr, 7)
+
+def cleanup(filelist):
+    """
+    Cleanup the intermediate files that were generated.
+    Method taken from:
+    http://stackoverflow.com/questions/10840533/most-pythonic-way-to-delete-a-file-which-may-not-exist
+    """
+    for f in filelist:
+        try:
+            os.remove(f)
+        except OSError as e:
+            if e.errno != errno.ENOENT: # errno.ENOENT = no such file or directory
+                raise # re-raise exception if a different error occured
 
 if __name__ == '__main__':
     def date2datetime(input_date, time_offset=time.min):
         if not input_date:
             return None
         return datetime.combine(input_date, time_offset)
-    
-    # Stacker class takes care of command line parameters
-    pqa_stacker = PQAStacker()
-    
-    if pqa_stacker.debug:
-        console_handler.setLevel(logging.DEBUG)
-    
+
+    stacker = Stacker()
+
     # Check for required command line parameters
-    assert pqa_stacker.x_index, 'Tile X-index not specified (-x or --x_index)'
-    assert pqa_stacker.y_index, 'Tile Y-index not specified (-y or --y_index)'
-    assert pqa_stacker.output_dir, 'Output directory not specified (-o or --output)'
-    
-    
-    stack_info_dict = pqa_stacker.stack_derived(x_index=pqa_stacker.x_index, 
-                         y_index=pqa_stacker.y_index, 
-                         stack_output_dir=pqa_stacker.output_dir, 
-                         start_datetime=date2datetime(pqa_stacker.start_date, time.min), 
-                         end_datetime=date2datetime(pqa_stacker.end_date, time.max), 
-                         satellite=pqa_stacker.satellite, 
-                         sensor=pqa_stacker.sensor)
-    
+    assert stacker.x_index, 'Tile X-index not specified (-x or --x_index)'
+    assert stacker.y_index, 'Tile Y-index not specified (-y or --y_index)'
+    assert stacker.output_dir, 'Output directory not specified (-o or --output)'
+    assert os.path.isdir(stacker.output_dir), 'Invalid output directory specified (-o or --output)'
+    stacker.output_dir = os.path.abspath(stacker.output_dir)
+
+    log_multiline(logger.debug, stacker.__dict__, 'stacker.__dict__', '\t')
+
+    # Stacker object already has command line parameters
+    # that disregard_incomplete_data is set to True for command line invokation
+    stack_info_dict = stacker.stack_tile(x_index=stacker.x_index,
+                                         y_index=stacker.y_index,
+                                         stack_output_dir=stacker.output_dir,
+                                         start_datetime=date2datetime(stacker.start_date, time.min),
+                                         end_datetime=date2datetime(stacker.end_date, time.max),
+                                         satellite=stacker.satellite,
+                                         sensor=stacker.sensor,
+                                         path=stacker.path,
+                                         row=stacker.row,
+                                         tile_type_id=None,
+                                         create_band_stacks=True,
+                                         disregard_incomplete_data=True)
+
     log_multiline(logger.debug, stack_info_dict, 'stack_info_dict', '\t')
-    logger.info('Finished creating %d temporal stack files in %s.', len(stack_info_dict), pqa_stacker.output_dir)
+    logger.info('Finished creating %d temporal stack files in %s.', len(stack_info_dict), stacker.output_dir)
+
+    file_paths = stack_info_dict.keys()
+    for pth in file_paths:
+        m = re.match(".*PQA.vrt", pth)
+        if m:
+            break
+
+    # Get info from the first band
+    b1 = stack_info_dict[pth][0]
+    x_idx = str(b1['x_index'])
+    y_idx = str(b1['y_index'])
+
+    cell = x_idx + '_' + y_idx
+
+    sat1, sat2, sat3, sat4, sat5, satTherm, sat7, prj, geoT = pqSaturation(pth)
+
+    summaries = [sat1, sat2, sat3, sat4, sat5, satTherm, sat7]
+
+    # Out filenames
+    out_fnames = ['Band_1_Saturation_Summary',
+                  'Band_2_Saturation_Summary',
+                  'Band_3_Saturation_Summary',
+                  'Band_4_Saturation_Summary',
+                  'Band_5_Saturation_Summary',
+                  'Therm_Saturation_Summary',
+                  'Band_7_Saturation_Summary']
+
+    out_fnames = [''.join([fname,'_', cell]) for fname in out_fnames]
+
+    out_fnames = [os.path.join(stacker.output_dir, fname) for fname in out_fnames]
+
+    # Write each summary to disk
+    for i in range(len(summaries)):
+        write_img(summaries[i], out_fnames[i], projection=prj, geotransform=geoT)
+
+    # Cleanup the intermediate files
+    print 'Removing files from disk:\n'
+    print file_paths
+    cleanup(file_paths)
+
+    #pdb.set_trace()
+
