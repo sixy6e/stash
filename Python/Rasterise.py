db3fbb887f62940dc67d4c956937dffba1c55a18
Josh Sixsmith <josh.sixsmith@gmail.com>
2014-07-20 17:32:02 +1000

    Deifned new functions projectVector, rasteriseVector. Defined the class segmentKeeper.

--- Python/Rasterise.py
+++ Python/Rasterise.py
@@ -8,15 +8,18 @@ from osgeo import osr
 from IDL_functions import histogram
 from IDL_functions import array_indices
 
-def createMemoryDataset(name='Rasterised', samples, lines, Projection=None, GeoTransform=None):
+def createMemoryDataset(name='MemoryDataset', samples, lines, Projection=None, GeoTransform=None):
     """
     
     """
 
+    # Define the Memory driver
     drv = gdal.GetDriverByName("MEM")
 
+    # Create the dataset
     outds = drv.Create(name, samples, lines, 1, gdal.GDT_UInt32)
 
+    # Set the projection and geotransfrom
     if Projection:
         outds.SetGeoTransform(GeoTransform)
     if GeoTransform:
@@ -24,13 +27,33 @@ def createMemoryDataset(name='Rasterised', samples, lines, Projection=None, GeoT
 
     return outds
 
-def projectVector(from_srs, to_srs):
-"""
+def projectVector(vectorLayer, from_srs, to_srs):
+    """
+    
+    """
 
-"""
-
+    # Define the transformation
     tform = osr.CoordinateTransformation(from_srs, to_srs)
-    geom.Transform(tform)
+
+    # Extract the geometry of every feature and transform it
+    # Transformation is done in place!!!
+    for feat in vectorLayer:
+        geom = feat.GetGeometryRef()
+        geom.Transform(tform)
+
+def rasteriseVector(imageDataset, vectorLayer):
+    """
+    
+    """
+
+    # Rasterise every feature based on it's FID value +1
+    for i in range(layer.GetFeatureCount()):
+        layer.SetAttributeFilter("FID = %d"%i)
+        burn = i + 1
+        gdal.RasterizeLayer(imageDataset, [1], vectorLayer, burn_values=[burn])
+        layer.SetAttributeFilter(None)
+
+    return imageDataset
 
 class Rasterise:
     """
@@ -52,6 +75,8 @@ class Rasterise:
         self._readVectorInfo()
 
         self.SameProjection = self.compareProjections(self.RasterInfo["Projection"], self.VectorInfo["Projection"])
+
+        self.segemented_array = None
 
     def _readRasterInfo(self):
         """
@@ -120,3 +145,160 @@ class Rasterise:
         """
         
         """
+
+        samples = self.RasterInfo["Samples"]
+        lines   = self.RasterInfo["Lines"]
+        proj    = self.RasterInfo["Projection"]
+        geot    = self.RasterInfo["GeoTransform"]
+
+        # Create the memory dataset into which the features will be rasterised
+        img_ds = createMemoryDataset(samples=samples, lines=lines, Projection=proj, 
+                     GeoTransform=proj)
+
+        # Open the vector dataset and retrieve the first layer
+        vec_ds = ogr.Open(self.VectorFname)
+        layer  = vec_ds.GetLayer(0)
+
+        if self.SameProjection:
+            # Rasterise the vector into image segments/rois
+            rasteriseVector(image_dataset=img_ds, vector_layer=layer)
+        else:
+            # Initialise the image and vector spatial reference
+            img_srs = osr.SpatialReference()
+            vec_srs = osr.SpatialReference()
+            img_srs.ImportFromWKT(proj)
+            vec_srs.ImportFromWKT(self.VectorInfo["Projection"])
+
+            # Project the vector
+            projectVector(layer, from_srs=vec_srs, to_srs=img_srs)
+
+            # Rasterise the vector into image segments/rois
+            rasteriseVector(image_dataset=img_ds, vector_layer=layer)
+
+        # Read the segmented array
+        self.segemented_array = img_ds.ReadAsArray()
+
+        # Close the image and vector datasets
+        img_ds = None
+        vec_ds = None
+
+class SegmentVisitor:
+    """
+    Given a segmented array, SegmentKeeper will find the segments and optionally
+    calculate basic statistics. A value of zero is considered to be the background
+    and ignored.
+    """
+
+    def __init__(self, array):
+        """
+        
+        """
+
+        self.array   = array
+        self.array1D = array.ravel()
+
+        self.dims = array.shape
+
+        self.histogram = None
+        self.ri        = None
+
+    def _findSegements(self)
+        """
+        
+        """
+
+        h = histogram(self.array1D, min=1, reverse_indices='ri')
+
+        self.histogram = h['histogram']
+        self.ri        = h['ri']
+
+    def getSegementData(self, array, segmentID=0):
+        """
+        Retrieve the data from an array corresponding to a segmentID.
+        """
+
+        ri       = self.ri
+        i        = segmentID
+        arr_flat = array.ravel()
+
+        if ri[i+1] > ri[i]:
+            data = arr_flat[ri[ri[i]:ri[i+1]]]
+        else:
+            data = numpy.array([])
+
+        return data
+
+    def getSegmentLocations(self, segmentID=0):
+        """
+        Retrieve the pixel locations corresponding to a segmentID.
+        """
+
+        ri = self.ri
+        i  = segmentID
+
+        if ri[i+1] > ri[i]:
+            idx = ri[ri[i]:ri[i+1]]
+        else:
+            idx = numpy.array([])
+
+        idx = array_indices(self.dims, idx, dimensions=True)
+
+        return idx
+
+    def segmentMean(self, array):
+        """
+        Calculates the mean value per segment given an array containing data.
+        """
+
+        arr_flat = array.ravel()
+        hist     = self.histogram
+        ri       = self.ri
+
+        mean_seg = {}
+
+        for i in numpy.arange(hist.shape[0]):
+            if (hist[i] == 0):
+                continue
+            xbar        = numpy.mean(arr_flat[ri[ri[i]:ri[i+1]]])
+            mean_seg[i] = xbar
+
+        return mean_seg
+
+     def segmentMax(self, array):
+        """
+        Calculates the max value per segment given an array containing data.
+        """
+
+        arr_flat = array.ravel()
+        hist     = self.histogram
+        ri       = self.ri
+
+        max_seg = {}
+
+        for i in numpy.arange(hist.shape[0]):
+            if (hist[i] == 0):
+                continue
+            mx_        = numpy.max(arr_flat[ri[ri[i]:ri[i+1]]])
+            max_seg[i] = mx_
+
+        return max_seg
+
+     def segmentMin(self, array):
+        """
+        Calculates the min value per segment given an array containing data.
+        """
+
+        arr_flat = array.ravel()
+        hist     = self.histogram
+        ri       = self.ri
+
+        min_seg = {}
+
+        for i in numpy.arange(hist.shape[0]):
+            if (hist[i] == 0):
+                continue
+            mn_        = numpy.max(arr_flat[ri[ri[i]:ri[i+1]]])
+            min_seg[i] = mn_
+
+        return min_seg
+
